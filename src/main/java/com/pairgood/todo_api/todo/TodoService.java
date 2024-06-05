f22e5779360093350a9cd0f31f11fc07d0c92297
Varshini Gadiraju <varshini.gadiraju@accenture.com>
2024-06-05 19:09:45 +0530

    varshini's Commit

--- src/main/java/com/pairgood/todo_api/todo/TodoService.java
+++ src/main/java/com/pairgood/todo_api/todo/TodoService.java
@@ -10,40 +10,43 @@ import java.util.logging.Logger;
 
 @Service
 public class TodoService {
-    private static Logger logger=Logger.getLogger(TodoService.class.getName());
+    private static Logger logger = Logger.getLogger(TodoService.class.getName());
     @Autowired
     private TodoRepository todoRepository;
 
     /**
      * Retrieve to-do list
+     *
      * @return
      */
-    public List<Todo> getMyTodoList(){
-        List<Todo> todoList= new ArrayList<>();
+    public List<Todo> getMyTodoList() {
+        List<Todo> todoList = new ArrayList<>();
         todoRepository.findAll().forEach(todoList::add);
 
-        return  todoList;
+        return todoList;
     }
 
 
     /**
      * retrieve to-do item
+     *
      * @param todoId
      * @return
      */
-    public Optional<Todo> GetTodoByID(long todoId){
+    public Optional<Todo> GetTodoByID(long todoId) {
         return todoRepository.findById(todoId);
     }
 
     /**
      * Add item to the list and return the id of the new item
+     *
      * @param todo
      * @return
      */
-    public long AddItemToThelist(Todo todo){
-        long todoId=0;
+    public long AddItemToThelist(Todo todo) {
+        long todoId = 0;
         todoRepository.save(todo);
-        todoId=todo.getTodoId();
+        todoId = todo.getTodoId();
 
         return todoId;
 
@@ -53,33 +56,35 @@ public class TodoService {
 
     /**
      * Delete item from the list
+     *
      * @param id
      */
-    public void DeleteItem(long id){
-        long itemId=0;
+    public void DeleteItem(long id) {
+        long itemId = 0;
         todoRepository.deleteById(id);
         logger.info("Item removed from the list");
     }
 
     /**
      * Update to-do item
+     *
      * @param todoId
      * @param todo
      * @return
      */
-    public long UpdateTodoItem(long todoId, Todo todo){
+    public long UpdateTodoItem(long todoId, Todo todo) {
 
-        long updateTodoId=0;
+        long updateTodoId = 0;
         //Retrieve the value you want to update
         try {
-            Todo updatedTodo=todoRepository.findById(todoId).get();
+            Todo updatedTodo = todoRepository.findById(todoId).get();
 
             updatedTodo.setTodoTitle(todo.getTodoTitle());
             updatedTodo.setTodoDescription(todo.getTodoDescription());
             updatedTodo.setTodoDate(todo.getTodoDate());
             updatedTodo.setComplete(todo.isComplete());
             todoRepository.save(updatedTodo);
-            updateTodoId=updatedTodo.getTodoId();
+            updateTodoId = updatedTodo.getTodoId();
             return updateTodoId;
         } catch (Exception e) {
             e.printStackTrace();
@@ -91,20 +96,22 @@ public class TodoService {
 
     /**
      * Verify if the id provided is valid
+     *
      * @param todoId
      * @return
      */
-    public boolean isTodoItemIdValid(long todoId){
+    public boolean isTodoItemIdValid(long todoId) {
         return todoRepository.findById(todoId).isPresent();
     }
 
 
     /**
      * returns number of items
+     *
      * @return
      */
-    public long getNumberTodoItem(){
-      return todoRepository.count();
+    public long getNumberTodoItem() {
+        return todoRepository.count();
     }
 
 
--- src/test/java/com/pairgood/todo_api/TodoApiApplicationTests.java
+++ src/test/java/com/pairgood/todo_api/TodoApiApplicationTests.java
@@ -1,13 +1,40 @@
 package com.pairgood.todo_api;
 
+import com.pairgood.todo_api.todo.ResponseTodoList;
+import com.pairgood.todo_api.todo.Todo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import java.time.LocalDate;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TodoApiApplicationTests {
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	@LocalServerPort
+	private int port;
 
 	@Test
-	void contextLoads() {
+	void contextLoads(){
+
 	}
+	@Test
+	void shouldCreateTodo(){
+		Todo todo= new Todo(0,"Spring Testing Session","Spring Testing",false, LocalDate.now(),null,null);
+		ResponseEntity<ResponseTodoList> response=testRestTemplate.postForEntity("http://localhost:"+port+"/api/v1/todo/additem",todo, ResponseTodoList.class);
+
+		response.getStatusCode();
+		Assertions.assertEquals(201,response.getStatusCode().value());
+
+
+
+	}
+
 
 }
